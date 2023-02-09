e9b92266cc587c80314bbd11f74a8449d7089d43
Matheus Alves <matheusfrancisco0330@gmail.com>
2023-02-09 10:41:54 -0300

    feat: customizando o theme

--- App.tsx
+++ App.tsx
@@ -6,18 +6,19 @@ import {
 } from "@expo-google-fonts/roboto";
 import { NativeBaseProvider } from "native-base";
 
+import { THEME } from "./src/theme";
 import { Loading } from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      <Loading/>
+      { fontsLoaded ? <View/> :  <Loading/>}
     </NativeBaseProvider>
   );
 }
