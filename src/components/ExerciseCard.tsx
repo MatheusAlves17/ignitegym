e209aebe98aa7012338697cb7dd291fe0f1cca7b
Matheus Alves <matheusfrancisco0330@gmail.com>
2023-02-10 10:23:17 -0300

    feat: componente de card dos exercícios

---
+++ src/components/ExerciseCard.tsx
@@ -0,0 +1,41 @@
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { Heading, HStack, Image, Text, VStack, Icon } from "native-base";
+import { Entypo } from '@expo/vector-icons';
+
+type TExerciseCardProps = TouchableOpacityProps & {};
+
+export function ExerciseCard({ ...rest }: TExerciseCardProps) {
+  return (
+    <TouchableOpacity {...rest}>
+      <HStack
+        bg="gray.500"
+        alignItems={"center"}
+        p={2}
+        pr={4}
+        mb={3}
+        rounded={"md"}
+      >
+        <Image
+          source={{
+            uri: "https://www.feitodeiridium.com.br/wp-content/uploads/2016/07/remada-unilateral-2.jpg",
+          }}
+          alt="Imagem do exercício"
+          h={16}
+          w={16}
+          mr={4}
+          rounded="md"
+          resizeMode="center"
+        />
+        <VStack flex={1}>
+          <Heading color="white" fontSize={"lg"}>
+            Remada unilateral
+          </Heading>
+          <Text color="gray.200" fontSize={"sm"} mt={1} numberOfLines={2}>
+            3 séries x 12 repetições
+          </Text>
+        </VStack>
+        <Icon as={Entypo} name='chevron-thin-right' color='gray.300'/>
+      </HStack>
+    </TouchableOpacity>
+  );
+}
