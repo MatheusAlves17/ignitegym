c69845679d8db461cbb5abebc1b4c4ccab9fd968
Matheus Alves <matheusfrancisco0330@gmail.com>
2023-02-10 12:28:51 -0300

    feat: finalizando interface de perfil do usuário

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,17 +1,27 @@
 import { useState } from "react";
-import { Center, ScrollView, VStack, Skeleton } from "native-base";
+import { TouchableOpacity } from "react-native";
+import {
+  Center,
+  ScrollView,
+  VStack,
+  Skeleton,
+  Text,
+  Heading,
+} from "native-base";
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserImage } from "@components/UserImage";
+import { Button } from "./Button";
+import { Input } from "./Input";
 
 export function Profile() {
   const PHOTO_SIZE = 33;
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
 
   return (
-    <VStack>
+    <VStack flex={1}>
       <ScreenHeader title="Perfil" />
-      <ScrollView>
+      <ScrollView contentContainerStyle={{ paddingBottom: 56 }}>
         <Center mt={6} px={10}>
           {photoIsLoading ? (
             <Skeleton
@@ -28,6 +38,37 @@ export function Profile() {
               source={{ uri: "https://github.com/rodrigorgtic.png" }}
             />
           )}
+          <TouchableOpacity>
+            <Text
+              color="green.500"
+              fontWeight="bold"
+              fontSize="md"
+              mt={2}
+              mb={8}
+            >
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+
+          <Input bg="gray.600" placeholder="Nome do usuário" />
+          <Input bg="gray.600" placeholder="E-mail" isDisabled />
+          <Heading
+            color="gray.200"
+            fontSize="md"
+            mb={2}
+            mt={12}
+            alignSelf="flex-start"
+          >
+            Alterar senha
+          </Heading>
+          <Input bg="gray.600" placeholder="Senha antiga" secureTextEntry />
+          <Input bg="gray.600" placeholder="Nova senha" secureTextEntry />
+          <Input
+            bg="gray.600"
+            placeholder="Confirme nova senha"
+            secureTextEntry
+          />
+          <Button title="Atualizar" mt={4} />
         </Center>
       </ScrollView>
     </VStack>
