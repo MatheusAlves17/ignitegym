09e7d75f7ae9f193377789a7de67af85f9396a12
Matheus Alves <matheusfrancisco0330@gmail.com>
2023-02-10 14:56:27 -0300

    feat: alterando foto de usuário

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -7,7 +7,11 @@ import {
   Skeleton,
   Text,
   Heading,
+  Toast,
+  useToast
 } from "native-base";
+import * as ImagePicker from "expo-image-picker";
+import * as FyleSystem from "expo-file-system";
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserImage } from "@components/UserImage";
@@ -16,8 +20,42 @@ import { Input } from "./Input";
 
 export function Profile() {
   const PHOTO_SIZE = 33;
+  const toast = useToast();
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState(
+    "https://github.com/rodrigorgtic.png"
+  );
 
+  async function handleUserPhotoSelect() {
+    setPhotoIsLoading(true);
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if (photoSelected.canceled) return;
+
+      if (photoSelected.assets[0].uri) {
+
+        const photoInfo = await FyleSystem.getInfoAsync(photoSelected.assets[0].uri)
+        if(photoInfo.size && (photoInfo.size / 1024 / 1024) > 5){
+          return toast.show({
+            title: 'Essa imagem é muito grande. Escolha uma de até 5MB',
+            placement: 'top',
+            bgColor: 'red.500'
+          })
+        }
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  }
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
@@ -35,10 +73,10 @@ export function Profile() {
             <UserImage
               alt="Foto do usuário"
               size={PHOTO_SIZE}
-              source={{ uri: "https://github.com/rodrigorgtic.png" }}
+              source={{ uri: userPhoto }}
             />
           )}
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               color="green.500"
               fontWeight="bold"
